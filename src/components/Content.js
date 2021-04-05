ad1af9cb0df394f362ac719b09b996a8d29258fb
Emmanuel Biolatiri <manuellaide49@gmail.com>
2021-04-05 02:17:13 +0100

    modified landing page

--- src/components/Content.js
+++ src/components/Content.js
@@ -11,43 +11,72 @@ import img3 from './swim3.jpeg'
 const Example = (props) => {
   return (
     <div>
-      <div className="mb-2">
-        <h1>Verified Instructors</h1>
+      <div className="my-5">
+        <h1>All Instructors</h1>
       </div>
        <Row>
         <Col xs="3">
           <SideForm />
         </Col>
         <Col xs="9">
-      <CardDeck>
-        <Card>
-          <CardImg top width="100%" src={img} alt="Card image cap" />
-          <CardBody>
-            <CardTitle tag="h5">Card title</CardTitle>
-            <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-            <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-            <Button color="primary">Button</Button>
-          </CardBody>
-        </Card>
-        <Card>
-          <CardImg top width="100%" src={img2} alt="Card image cap" />
-          <CardBody>
-            <CardTitle tag="h5">Card title</CardTitle>
-            <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-            <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-            <Button color="primary">Button</Button>
-          </CardBody>
-        </Card>
-        <Card>
-          <CardImg top width="100%" src={img3} alt="Card image cap" />
-          <CardBody>
-            <CardTitle tag="h5">Card title</CardTitle>
-            <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-            <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-            <Button color="primary">Button</Button>
-          </CardBody>
-        </Card>
-          </CardDeck>
+          <CardDeck className="mb-4">
+              <Card>
+                <CardImg top width="100%" src={img} alt="Card image cap" />
+                <CardBody>
+                  <CardTitle tag="h5">Card title</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+                  <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+                  <Button color="primary">Button</Button>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardImg top width="100%" src={img2} alt="Card image cap" />
+                <CardBody>
+                  <CardTitle tag="h5">Card title</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+                  <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
+                  <Button color="primary">Button</Button>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardImg top width="100%" src={img3} alt="Card image cap" />
+                <CardBody>
+                  <CardTitle tag="h5">Card title</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+                  <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
+                  <Button color="primary">Button</Button>
+                </CardBody>
+                </Card>
+            </CardDeck>
+          <CardDeck>
+              <Card>
+                <CardImg top width="100%" src={img} alt="Card image cap" />
+                <CardBody>
+                  <CardTitle tag="h5">Card title</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+                  <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+                  <Button color="primary">Button</Button>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardImg top width="100%" src={img2} alt="Card image cap" />
+                <CardBody>
+                  <CardTitle tag="h5">Card title</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+                  <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
+                  <Button color="primary">Button</Button>
+                </CardBody>
+              </Card>
+              <Card>
+                <CardImg top width="100%" src={img3} alt="Card image cap" />
+                <CardBody>
+                  <CardTitle tag="h5">Card title</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+                  <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
+                  <Button color="primary">Button</Button>
+                </CardBody>
+                </Card>
+            </CardDeck>
           </Col>
       </Row>
       </div>
